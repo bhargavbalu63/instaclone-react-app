549504150d936ba0258b84d27735313913e97257
L BHARGAV <bhargavbalu63@gmail.com>
2022-12-18 11:11:44 +0530

    added insta-react

---
+++ src/PostView/PostView.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import Card from './Card'
+import Navbar from './Navbar'
+import axios from 'axios'
+
+export default function PostVIew() {
+
+    const [userData, setUserData] = useState('')
+
+    function fetchPosts() {
+        axios
+            .get('')
+            .then(res => {
+                let dataArr = res.data
+                dataArr.reverse()
+                setUserData(dataArr)
+            })
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+
+
+    return <>
+        <div className='postview-container'>
+            <Navbar />
+            {!userData.length ?
+                <h1 className='while-loading'>Loading Posts...</h1>
+                :
+                <div className='card-container'>
+                    {userData.map((data, idx) => {
+                        return <Card key={idx} data={data} />
+                    })}
+                </div>}
+        </div>
+    </>
+}
